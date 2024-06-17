f29e4d8e1102e6b541a396a2a1f22f1501a2d576
nassor-j-a <rossan1tech@gmail.com>
2024-06-18 00:33:49 +0300

    about page

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -6,3 +6,6 @@ def home(request):
     products = Product.objects.all()
     
     return render(request, 'home.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
