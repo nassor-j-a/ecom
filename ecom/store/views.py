1c10f587b3287edcaa524792863ed0389780ca2f
nassor-j-a <rossan1tech@gmail.com>
2024-06-18 22:44:12 +0300

    Adding login and logout functions

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages # This is for messages in the login_user function
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,23 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in...Welcome"))
+            return redirect('home')
+        else:
+            messages.error(request, ("Invalid username or password"))
+            return render(request, 'login.html', {})
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out...Thanks for stopping by"))
+    return redirect('home')
