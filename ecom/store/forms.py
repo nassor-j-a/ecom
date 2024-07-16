2cd5e1d6d89c4ab5711ab584c15d83a2aede0bc5
nassor-j-a <rossan1tech@gmail.com>
2024-07-16 23:50:53 +0300

    User Profile Page

--- ecom/store/forms.py
+++ ecom/store/forms.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPasswordForm
 from django import forms
+from .models import Profile
+
+class UserInfoForm(forms.ModelForm):
+    phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}), required=False)
+    address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address 1'}), required=False)
+    address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address 2'}), required=False)
+    city = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}), required=False)
+    state = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}), required=False)
+    zip_code = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zipcode'}), required=False)
+    country = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}), required=False)
+    
+    class Meta:
+      model = Profile
+      fields = ('phone', 'address1', 'address2', 'city', 'state', 'zip_code', 'country', )
 
 class  ChangePasswordForm(SetPasswordForm):
     class Meta:
@@ -62,11 +76,11 @@ class UpdateUserForm(UserChangeForm):
     password = None
     # Get other fields
     email = forms.EmailField(label="", widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+        attrs={'class': 'form-control', 'placeholder': 'Email Address'}), required=False)
     first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+        attrs={'class': 'form-control', 'placeholder': 'First Name'}), required=False)
     last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+        attrs={'class': 'form-control', 'placeholder': 'Last Name'}), required=False)
 
     class Meta:
         model = User
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 
 # This is for messages in the login_user function
@@ -7,7 +7,7 @@ from django.contrib import messages
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 
 # Create your views here.
@@ -58,8 +58,8 @@ def register_user(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(
-                request, ("You have Registered Successfully!!!"))
-            return redirect('home')
+                request, ("Username created - Please Fill Out Your User Info Below..."))
+            return redirect('update_info')
         else:
             messages.success(
                 request, ("There wa aproblem registering, please try again!!!"))
@@ -108,6 +108,19 @@ def update_user(request):
         messages.success(request, "You must be must be logged In to access that page !!!")
         return redirect('home')
 
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Your Info Has Been Updated!!!")
+            return redirect('home')
+        return render(request, 'update_info.html', {'form' : form})
+    else:
+        messages.success(request, "You must be must be logged In to access that page !!!")
+        return redirect('home')
 
 
 def product(request, pk):
